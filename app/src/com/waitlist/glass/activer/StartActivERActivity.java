458b8507037a8bebdae2d5b56928da3bb74a8148
davelobsinger <Dave@Daves-MacBook-Pro.local>
2014-09-19 12:09:24 -0400

    Android project creation
    Created a new project for the Google Glass app of ActivER

---
+++ app/src/com/waitlist/glass/activer/StartActivERActivity.java
@@ -0,0 +1,15 @@
+package com.waitlist.glass.activer;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class StartActivERActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_start_activ_er);
+	}
+}
